ca1376271ac4b805fe8165b0c1532ec8fba8e382
Fabrice Semti <fabrice@fabricesemti.com>
2026-07-06 20:07:19 +0100

    feat: manage tailscale ACLs and service routes via terraform
    - Add tailscale provider and ACL resource for tagOwners/autoApprovers
    - Configure docker-svc-0 tailscale SSH, exit-node, and subnet routes
    - Fix ansible sudo timeout with become_flags=-n
    - Reset tailscale state on docker env/compose changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,3 +4,8 @@ provider "proxmox" {
   username = var.proxmox_username
   password = var.proxmox_password
 }
+
+provider "tailscale" {
+  oauth_client_id     = var.tailscale_oauth_client_id
+  oauth_client_secret = var.tailscale_oauth_client_secret
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,3 +26,14 @@ variable "root_ssh_public_keys" {
   description = "SSH public keys to install for root in managed guests."
   default     = []
 }
+
+variable "tailscale_oauth_client_id" {
+  type        = string
+  description = "Tailscale OAuth client ID for managing ACLs."
+}
+
+variable "tailscale_oauth_client_secret" {
+  type        = string
+  description = "Tailscale OAuth client secret for managing ACLs."
+  sensitive   = true
+}
